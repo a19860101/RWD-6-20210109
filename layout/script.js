8577f2943ec5009562de3f35d83edd5fae962e53
Zac <a19860101@gmail.com>
2021-04-03 11:52:25 +0800

    20210403

--- layout/script.js
+++ layout/script.js
@@ -1,10 +1,25 @@
 $(function(){
-    $('.menu').find('a').click(function(){
+    $('.menu').find('a').click(function(){    
         let target = $(this).data('target');
+        if(target == '#search'){return;}
         let distance = $(target).offset().top;
         let nav_h = $('nav').outerHeight();
         $('html,body').animate({
             scrollTop: distance - nav_h
+        })
+    })
+    $(window).scroll(function(){
+        let h = $(this).scrollTop();
+        console.log(h);
+        
+        $('.menu').find('a').each(function(){
+            let target = $(this).data('target');
+            if(target == '#search'){return;}
+            let distance = $(target).offset().top;
+            if(h > distance - 100){
+                $('.menu').find('a').removeClass('active');
+                $(this).addClass('active');
+            }
         })
     })
 })
